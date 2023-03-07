6f60a3f18d6332cf65bddfbd6350615f4c34a37e
hithere95 <125794457+hithere95@users.noreply.github.com>
2023-03-07 22:02:06 +0100

    Add files via upload

---
+++ BOOTSTRAP/javakod.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+var mojaFunk = () => {
+
+    var broj = prompt("Unesite broj od 1 do 5");
+    var x =(broj)
+
+  
+        if (x > 0 && x <= 5) {
+            var citat = " Kreativan čovek motivisan je željom da postigne, a ne željom da pobedi druge. ";
+            document.write= (citat + "<br>" )
+            }
+           if (isNaN(x)) {
+                alert("Unesen broj mora biti izmedju 1 i 5!"); 
+            }
+       
+    if (x>5) {
+        alert("Niste uneli broj!");
+    }
+
+}
---
+++ JScode.js
@@ -0,0 +1,61 @@
+function myFunk() {
+    var broj = prompt("Insert number between 1 and 999");
+    var x = (broj)
+    if (x > 0 && x <= 999) {
+       var sqrt =  Math.sqrt(x);
+    alert(sqrt)
+    }
+    if (isNaN(x)) {
+       alert("This is not a number!")
+    }
+    if (x>999) {
+       alert("Number must be between 1 i 999!");
+    }
+ }
+ 
+ function  myFunk1() {
+    var broj = prompt("Insert number between 1 and 999");
+    var x = (broj)
+    if (x > 0 && x <= 999) {
+       var sin =  Math.sin(x);
+    alert(sin)
+    }
+    if (isNaN(x)) {
+       alert("This is not a number!")
+    }
+    if (x>999) {
+       alert("Number must be between 1 i 999!");
+    }
+ }
+ 
+ function myFunk2() {
+    var broj = prompt("Insert number between 1 and 999");
+    var x = (broj)
+    if (x > 0 && x <= 999) {
+       var cos =  Math.cos(x);
+    alert(cos)
+    }
+    if (isNaN(x)) {
+       alert("This is not a number!")
+    }
+    if (x>999) {
+       alert("Number must be between 1 i 999!");
+    }
+ }
+ 
+ 
+ function myFunk3() {
+    var broj = prompt("Insert number between 1 and 999");
+    var x = (broj)
+    if (x > 0 && x <= 999) {
+       var round =  Math.round (x);
+    alert(round)
+    }
+    if (isNaN(x)) {
+       alert("This is not a number!")
+    }
+    if (x>999) {
+       alert("Number must be between 1 i 999!");
+    }
+ }
+ 
